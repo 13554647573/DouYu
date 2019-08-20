5e0d374142a2105664e06e3bc17aaa182a5d53c1
13554647573@163.com <xili159753>
2019-08-20 15:55:03 +0800

    创建标签控制器

---
+++ DouYu/DouYu/Classes/Live/LiveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LiveViewController.swift
+//  DouYu
+//
+//  Created by 李   胜 on 2019/8/20.
+//  Copyright © 2019 Wuhan Xueyetong Big Data  Institute Co. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class LiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DouYu/DouYu/Classes/Main/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  DouYu
+//
+//  Created by 李   胜 on 2019/8/20.
+//  Copyright © 2019 Wuhan Xueyetong Big Data  Institute Co. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.white
+        
+//        addChild(HomeViewController())
+//        addChild(LiveViewController())
+//        addChild(FollowViewController())
+//        addChild(ProfileViewController())
+    }
+
+}
